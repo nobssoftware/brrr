2d68c8858b91be8e3d9d74a40da100612451b949
Hraban Luyat <hraban@0brg.net>
2024-12-16 23:23:40 -0500

    chore: remove unused asyncrrr.py

--- src/brrr/asyncrrr.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-async def async_wrrrk(workers: int = 1):
-    """
-    Spin up a number of worker threads
-    """
-    return asyncio.gather(
-        *[asyncio.create_task(asyncio_worker()) for _ in range(workers)]
-    )
